85cf37d8abc3e58146f80b6dbe3dc832b3515302
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-09-23 04:32:07 +0100

    read and write to path

---
+++ Assignment/ReadingAndWriting/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+const string filepath = @"C:\Users\HP-PC\OneDrive\Desktop\C# bootcamp\cSharp\Assignment\message.txt";
+
+Console.WriteLine("Enter the words to be stored in the file");
+
+string? elements = Console.ReadLine();
+File.WriteAllText(filepath, elements);
+string text = File.ReadAllText(filepath);
+
+Console.WriteLine(text);
+
