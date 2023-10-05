d34b0cd0aa5457f534bd07afcbbf0011f002bd69
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-05 22:54:26 +0100

    12

--- Day12/Example4/Program.cs
+++ Day12/Example4/Program.cs
@@ -8,3 +8,8 @@ myDictionary.Add(3, "Isiaq");
 myDictionary.Add(4, "IRB");
 myDictionary.Add(5, "Bukky");
 
+foreach (KeyValuePair<int, string> item in myDictionary)
+{
+    Console.WriteLine($"Key {item.Key}: value {item.Value}");
+    Console.WriteLine();
+}
