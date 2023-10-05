8ab753162ecb08bc89ddae4880303ba658625915
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-05 22:49:37 +0100

    12

--- Day12/Example4/Program.cs
+++ Day12/Example4/Program.cs
@@ -1,13 +1,1 @@
-﻿using System.Collections.Generic;
-
-Dictionary<int, string> myDictionary = new Dictionary<int, string>();
-
-myDictionary.Add(1, "Ridwan");
-myDictionary.Add(2, "Bukola");
-myDictionary.Add(3, "Isiaq");
-myDictionary.Add(4, "IRB");
-
-foreach (KeyValuePair<int, string> item in myDictionary)
-{
-    Console.WriteLine($"Key {item.Key}: value {item.Value}");
-}
+﻿
