873208c8199164077680c3c68799d1e3913f42f6
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-28 14:45:26 +0100

    connected to sql db

--- Day17/Example1/Program.cs
+++ Day17/Example1/Program.cs
@@ -1,3 +1,31 @@
-﻿using System.Data;
+﻿
+using System.Data.SqlClient;
 
-Console.WriteLine("HELLO WORLD");
+const string dbConnectionString = "Server=localhost;Database=institute;Trusted_Connection=True;";
+
+string sql = "SELECT * FROM institute.dbo.students";
+
+SqlConnection connection = new SqlConnection(dbConnectionString);
+
+SqlCommand cmd = new SqlCommand();
+cmd.Connection = connection;
+
+cmd.CommandText = sql;
+
+connection.Open();
+SqlDataReader reader = cmd.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine("=====================");
+
+    Console.WriteLine($"Id: {reader.GetInt32(0)}");
+    Console.WriteLine($"Name: {reader.GetString(1)}");
+    Console.WriteLine($"Country: {reader.GetString(2)}");
+    Console.WriteLine($"State: {reader.GetString(3)}");
+    Console.WriteLine($"City: {reader.GetString(4)}");
+
+    Console.WriteLine("=====================");
+}
+
+connection.Close();
