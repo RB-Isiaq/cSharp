90ebcdaf350560ca093d726c552adeadfe921452
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-31 10:27:58 +0100

    updating data from c#

---
+++ Day18/Example2/Data/InstitutionData.cs
@@ -0,0 +1,160 @@
+using System.Data;
+using System.Data.SqlClient;
+using Example2.Entities;
+
+namespace Example2.Data
+{
+    public class InstitutionData
+    {
+        private readonly String _dbConnectionString;
+
+        public InstitutionData(String dbConnectionString)
+        {
+            _dbConnectionString = dbConnectionString;
+        }
+        public bool AddInstitution(Institution institution)
+        {
+            SqlConnection connection = new SqlConnection(_dbConnectionString);
+
+            string insertStatement = "INSERT INTO students (NAME, COUNTRY, STATE, CITY) VALUES (@NAME, @COUNTRY, @STATE, @CITY)";
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = insertStatement;
+
+            SqlParameter paramName = new SqlParameter();
+            paramName.ParameterName = "@NAME";
+            paramName.SqlDbType = SqlDbType.VarChar;
+            paramName.Direction = ParameterDirection.Input;
+            paramName.Value = institution.Name;
+            command.Parameters.Add(paramName);
+
+            SqlParameter paramCountry = new SqlParameter();
+            paramCountry.ParameterName = "@COUNTRY";
+            paramCountry.SqlDbType = SqlDbType.VarChar;
+            paramCountry.Direction = ParameterDirection.Input;
+            paramCountry.Value = institution.Country;
+            command.Parameters.Add(paramCountry);
+
+            SqlParameter paramState = new SqlParameter();
+            paramState.ParameterName = "@STATE";
+            paramState.SqlDbType = SqlDbType.VarChar;
+            paramState.Direction = ParameterDirection.Input;
+            paramState.Value = institution.State;
+            command.Parameters.Add(paramState);
+
+            SqlParameter paramCIty = new SqlParameter();
+            paramCIty.ParameterName = "@CITY";
+            paramCIty.SqlDbType = SqlDbType.VarChar;
+            paramCIty.Direction = ParameterDirection.Input;
+            paramCIty.Value = institution.City;
+            command.Parameters.Add(paramCIty);
+
+            connection.Open();
+            int result = command.ExecuteNonQuery();
+            Console.WriteLine(result);
+            connection.Close();
+
+            return true;
+        }
+        public bool UpdateInstitution(Institution institution)
+        {
+            SqlConnection connection = new SqlConnection(_dbConnectionString);
+
+            string insertStatement = "UPDATE students SET NAME = @NAME,  COUNTRY = @COUNTRY,  STATE = @STATE,  CITY = @CITY WHERE ID = @ID";
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = insertStatement;
+
+            SqlParameter paramID = new SqlParameter();
+            paramID.ParameterName = "@ID";
+            paramID.SqlDbType = SqlDbType.VarChar;
+            paramID.Direction = ParameterDirection.Input;
+            paramID.Value = institution.Id;
+            command.Parameters.Add(paramID);
+
+            SqlParameter paramName = new SqlParameter();
+            paramName.ParameterName = "@NAME";
+            paramName.SqlDbType = SqlDbType.VarChar;
+            paramName.Direction = ParameterDirection.Input;
+            paramName.Value = institution.Name;
+            command.Parameters.Add(paramName);
+
+            SqlParameter paramCountry = new SqlParameter();
+            paramCountry.ParameterName = "@COUNTRY";
+            paramCountry.SqlDbType = SqlDbType.VarChar;
+            paramCountry.Direction = ParameterDirection.Input;
+            paramCountry.Value = institution.Country;
+            command.Parameters.Add(paramCountry);
+
+            SqlParameter paramState = new SqlParameter();
+            paramState.ParameterName = "@STATE";
+            paramState.SqlDbType = SqlDbType.VarChar;
+            paramState.Direction = ParameterDirection.Input;
+            paramState.Value = institution.State;
+            command.Parameters.Add(paramState);
+
+            SqlParameter paramCIty = new SqlParameter();
+            paramCIty.ParameterName = "@CITY";
+            paramCIty.SqlDbType = SqlDbType.VarChar;
+            paramCIty.Direction = ParameterDirection.Input;
+            paramCIty.Value = institution.City;
+            command.Parameters.Add(paramCIty);
+
+            connection.Open();
+            int result = command.ExecuteNonQuery();
+            Console.WriteLine(result);
+            connection.Close();
+
+            return true;
+        }
+
+        public List<Institution> GetInstitutions()
+        {
+            SqlConnection connection = new SqlConnection(_dbConnectionString);
+
+            List<Institution> institutions = new List<Institution>();
+
+
+            string sql = "SELECT * FROM students";
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = connection;
+
+            cmd.CommandText = sql;
+
+            try
+            {
+                connection.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Institution institution = new Institution();
+                    institution.Id = reader.GetInt32(reader.GetOrdinal("ID"));
+                    institution.Name = reader.GetString(reader.GetOrdinal("NAME"));
+                    institution.Country = reader.GetString(reader.GetOrdinal("COUNTRY"));
+                    institution.State = reader.GetString(reader.GetOrdinal("STATE"));
+                    institution.City = reader.GetString(reader.GetOrdinal("CITY"));
+
+                    institutions.Add(institution);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Database operation failed: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("SOmething went wrong: " + ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            return institutions;
+        }
+    }
+}
---
+++ Day18/Example2/Program.cs
@@ -0,0 +1,86 @@
+﻿using Example2.Data;
+using Example2.Entities;
+
+namespace Example2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What do youo want to do?");
+        GetInstitutions();
+    }
+
+    private static void CreateInstitution()
+    {
+
+        Institution institution = new Institution();
+        Console.WriteLine("Please provide institution's name");
+
+        institution.Name = Console.ReadLine();
+        Console.WriteLine("Please provide institution's country");
+        institution.Country = Console.ReadLine();
+
+        Console.WriteLine("Please provide institution's state");
+        institution.State = Console.ReadLine();
+
+        Console.WriteLine("Please provide institution's city");
+        institution.City = Console.ReadLine();
+
+        InstitutionData institutionData = new InstitutionData("Server=localhost;Database=institute;Trusted_Connection=True;");
+        bool result = institutionData.AddInstitution(institution);
+
+        if (result)
+        {
+            Console.WriteLine("Institution added successfully");
+        }
+    }
+
+    private static void UpdateInstitution()
+    {
+        Institution institution = new Institution();
+
+        Console.WriteLine("Please provide institution's id");
+        institution.Id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Please provide institution's name");
+        institution.Name = Console.ReadLine();
+
+        Console.WriteLine("Please provide institution's country");
+        institution.Country = Console.ReadLine();
+
+        Console.WriteLine("Please provide institution's state");
+        institution.State = Console.ReadLine();
+
+        Console.WriteLine("Please provide institution's city");
+        institution.City = Console.ReadLine();
+
+        InstitutionData institutionData = new InstitutionData("Server=localhost;Database=institute;Trusted_Connection=True;");
+        bool result = institutionData.UpdateInstitution(institution);
+
+        if (result)
+        {
+            Console.WriteLine("Institution updated successfully");
+        }
+    }
+
+    public static void GetInstitutions()
+    {
+        InstitutionData institutionData = new InstitutionData("Server=localhost;Database=institute;Trusted_Connection=True;");
+
+        List<Institution> institutions = institutionData.GetInstitutions();
+
+        foreach (Institution institution in institutions)
+        {
+            Console.WriteLine("=====================");
+
+            Console.WriteLine($"Id: {institution.Id}");
+            Console.WriteLine($"Name: {institution.Name}");
+            Console.WriteLine($"City: {institution.City}");
+            Console.WriteLine($"State: {institution.State}");
+            Console.WriteLine($"Country: {institution.Country}");
+
+            Console.WriteLine("=====================");
+        }
+    }
+}
